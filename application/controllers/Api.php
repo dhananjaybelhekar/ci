5654a2bf88d6c5e38bc14c4b7da8737161f52d77
dhananjay431 <dbel431@gmail.com>
2017-11-17 14:26:20 +0530

    api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -5,38 +5,27 @@ class Api extends CI_Controller {
 
 	public function __construct()
         {
-        header('Access-Control-Allow-Origin: *');
+		parent::__construct();
+				header('Access-Control-Allow-Origin: *');
                 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
                 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
                 $method = $_SERVER['REQUEST_METHOD'];
                 if($method == "OPTIONS") {
                 die();
                 }
+		$this->load->database();
         }
 
 	public function index()
 	{
 		echo json_encode(array('foo'=>456465,'bar'=>8879878));
 	}
-		public function logout()
-	{
-		$this->load->database();
-		$req = json_decode($this->input->raw_input_stream);	
-		if(isset($req->result))
-		{
-			$qr =$this->db->delete('log', array('fingerprint' =>$req->result));
-			// $this->db->query($qr);
-			$arr= array();
-			if($qr){
-				$arr['url']= 'login';		
-				$arr['success']= true;
-			}
-			echo json_encode($arr);
-		}
-	}
+  // $this->load does not exist until after you call this
+        parent::__construct(); // Construct CI's core so that you can use it
+
+ 
 	public function login()
 	{
-		$this->load->database();
 		$arr = $this->db->query("select *from log")->result_array();
 		echo json_encode($arr);
 	}
